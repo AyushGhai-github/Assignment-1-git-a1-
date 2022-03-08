b897138fb017dc2461349c7e1b52b08761ddf0bf
AyushGhai-github <97190388+AyushGhai-github@users.noreply.github.com>
2022-03-08 07:16:35 +0530

    Add files via upload

---
+++ Ts-Script/Es6-Assignmnet/Q4.ts
@@ -0,0 +1,1 @@
+let names = ["Tom", "Ivan", "Jerry"];
---
+++ Ts-Script/Es6-Assignmnet/Q7.ts
@@ -0,0 +1,20 @@
+/*a)Suppose there is a javascript array with 4 elements.
+ Print the value of 3rd element using array matching.*/
+
+ let arr=["Ayush","Poonam","Atharv","Omkar"];
+ let[Ay,Po,At,Om]=arr;
+ console.log("value of 3rd element is"+At);
+
+
+ /*b.Create an organization object having 
+ attributes name, address.
+  Write a program to retrieve pin code of an address 
+  using object deep matching.*/
+
+  let pinCode=44001;
+  let organization={
+      name:"Ayush",
+      address:"KAsturchand Park",
+      pinCode
+  }
+  console.log("Pincode is"+pinCode);
