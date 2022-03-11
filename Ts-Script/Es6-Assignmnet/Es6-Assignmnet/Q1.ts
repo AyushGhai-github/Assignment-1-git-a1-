86807bf2cba0382d0ccc640ea9743a1fe4579933
AyushGhai-github <97190388+AyushGhai-github@users.noreply.github.com>
2022-03-11 09:07:36 +0530

    Add files via upload

---
+++ Ts-Script/Es6-Assignmnet/Es6-Assignmnet/Q1.ts
@@ -0,0 +1,25 @@
+const number = 40;
+try {
+  number = 99;
+} catch (err) {
+  console.log(err);
+}
+
+console.log(number);
+// expected output: 42
+
+
+
+//Assigning Object
+const obj={
+    name:"ayush"
+};
+console.log(obj.name);//op will be ayush
+obj.name="Atharv";
+console.log(obj.name);//op will be atharv
+//Note with the help of consatnt we can assign a new value but not a new obj with sam name
+
+//Trying to creating obj with same name
+// const obj={//it will produce an error Identifier 'obj' has al}
+//     name:"gopu"
+// };
