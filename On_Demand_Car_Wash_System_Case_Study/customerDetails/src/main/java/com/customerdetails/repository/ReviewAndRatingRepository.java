aa8f78f43c7b2aabd18d479c2119132a2614ae47
AyushGhai-github <97190388+AyushGhai-github@users.noreply.github.com>
2022-04-29 00:09:52 +0530

    Delete On_Demand_Car_Wash_System_Case_Study/customerDetails directory

--- On_Demand_Car_Wash_System_Case_Study/customerDetails/src/main/java/com/customerdetails/repository/ReviewAndRatingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.customerdetails.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.customerdetails.model.ReviewAndRating;
-
-public interface ReviewAndRatingRepository  extends MongoRepository<ReviewAndRating,Integer>{
-
-}
